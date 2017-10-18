d31e5f7267bceda97e4dfb8108624ffabcbabe5f
Luca Cinquini <cinquini@LMC-038261.local>
2017-10-18 06:41:50 -0600

    Moving all site specific configuration to an external file.

--- esgf_auth/settings.py
+++ esgf_auth/settings.py
@@ -11,23 +11,27 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# site-specific configuration
+ESGF_AUTH_CONFIG_FILE = os.environ.get('ESGF_AUTH_CONFIG_FILE','/esg/config/esgf_auth_config.json')
+with open(ESGF_AUTH_CONFIG_FILE) as json_data_file:
+    config_data = json.load(json_data_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5_7$jn1-c8h5@(e$x8fhk=&!mk=ea6=irwm+b22nu)h=rhox46'
+SECRET_KEY = config_data['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 #DEBUG =False
 
-ALLOWED_HOSTS = [ u'dev.esgf.anl.gov', ]
-
+ALLOWED_HOSTS = [ config_data['ESGF_HOSTNAME'] ]
 
 # Application definition
 
@@ -100,10 +104,10 @@ SOCIAL_AUTH_SANITIZE_REDIRECTS = False
 SOCIAL_AUTH_ESGF_AUTH_EXTRA_ARGUMENTS = {\
     'access_type': 'offline',
 }
-ESGF_OAUTH2_SECRET_FILE = '/esg/config/esgf_oauth2.json'
+ESGF_OAUTH2_SECRET_FILE = os.environ.get('ESGF_OAUTH2_SECRET_FILE', '/esg/config/esgf_oauth2.json')
 ESGF_RETURN_QUERY_NAME = 'redirect'
 ESGF_SESSION_COOKIE_NAME = 'session-cookie'
-ESGF_SECRET_KEY = 'xnVuDEZROQfoBT+scRkaig=='
+ESGF_SECRET_KEY = config_data['ESGF_SECRET_KEY']
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
